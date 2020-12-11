ee6bdb01aa65d275d6119d61b420be0513c8e312
Jack Standbridge <jack.s.standbridge@gmail.com>
2020-12-11 14:40:41 +0000

    add placeholder ipsum project

---
+++ components/PlaceholderIpsum/PlaceholderIpsum.tsx
@@ -0,0 +1,85 @@
+import { FC } from 'react';
+import Head from 'next/head'
+
+import styles from './PlaceholderIpsum.module.scss';
+
+interface Props {
+	handleSentences: (e: React.ChangeEvent<HTMLInputElement>) => void,
+	handleParagraphs: (e: React.ChangeEvent<HTMLInputElement>) => void,
+	numberOfSentences: number,
+	numberOfParagraphs: number,
+	output: string[][],
+	handleCopy: () => void,
+	buttonText: string,
+}
+
+const PlaceholderIpsum: FC<Props> = ({
+	handleSentences,
+	handleParagraphs,
+	numberOfSentences,
+	numberOfParagraphs,
+	handleCopy,
+	output,
+	buttonText,
+}) => {
+	return (
+		<div className='site-width'>
+			<Head>
+				<title>Placeholder Ipsum</title>
+			</Head>
+
+			<header className={ styles.header }>
+				<h1>Placeholder Ipsum</h1>
+				<p className={ styles.paragraph }>A simple Lorem Ipsum generator that explains that the content is just placeholder text.</p>
+			</header>
+
+			<main className={ styles.main }>
+				<section className={ styles.inputs }>
+					<label className={ styles.label }>
+						Sentences per paragraph:
+					<input
+							className={ styles.input }
+							type='number'
+							value={ numberOfSentences }
+							onChange={ handleSentences }
+						/>
+					</label>
+
+					<label className={ styles.label }>
+						How many paragraphs:
+					<input
+							className={ styles.input }
+							type='number'
+							value={ numberOfParagraphs }
+							onChange={ handleParagraphs }
+						/>
+					</label>
+				</section>
+
+				<article>
+					<header className={ styles.outputHeader }>
+						<h2>Your Placeholder Ipsum:</h2>
+						<button
+							onClick={ handleCopy }
+							className={ styles.button }
+						>
+							{ buttonText }
+						</button>
+					</header>
+
+					<div className={ styles.ipsum }>
+						{ output.map((paragraph, i) => (
+							<p className={ styles.paragraph } key={ i }>
+								{ paragraph.join(' ') }
+							</p>
+						)) }
+					</div>
+				</article>
+
+			</main>
+
+		</div>
+	)
+};
+
+export default PlaceholderIpsum;
---
+++ components/PlaceholderIpsum/index.tsx
@@ -0,0 +1,70 @@
+import { FC, useState, useCallback, useEffect } from 'react';
+import PlaceholderIpsum from './PlaceholderIpsum';
+import sentences from '../../lib/data/sentences.json';
+import { clamp } from '../../lib/utils';
+
+const ConnectedPlaceholderIpsum: FC = () => {
+	const [numberOfSentences, setNumberOfSentences] = useState<number>(8);
+	const [numberOfParagraphs, setNumberOfParagraphs] = useState<number>(3);
+
+	const handleParagraphs = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setNumberOfParagraphs(clamp(1, Infinity, +e.currentTarget.value));
+	};
+
+	const handleSentences = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setNumberOfSentences(clamp(1, Infinity, +e.currentTarget.value));
+	};
+
+	const [output, setOutput] = useState<string[][]>([]);
+
+	useEffect(() => {
+		const result = Array(Math.abs(numberOfParagraphs)).fill('').map(() => {
+			return Array(Math.abs(numberOfSentences)).fill('').map(() => {
+				const index = Math.floor(Math.random() * sentences.length);
+				return sentences[index];
+			});
+		});
+
+		result[0][0] = sentences[0];
+
+		setOutput(result);
+
+	}, [setOutput, numberOfSentences, numberOfParagraphs]);
+
+	const [buttonText, setButtonText] = useState('Copy');
+
+	const handleCopy = useCallback(async () => {
+		let html = '';
+		html += output.map(paragraph => {
+			return paragraph.join(' ');
+		}).join('\n\n');
+
+		try {
+			await window.navigator.clipboard.writeText(html);
+			setButtonText('Copied!');
+
+		} catch (e) {
+			setButtonText(
+				'Your browser doesn\'t support copying to the clipboard'
+			);
+
+		} finally {
+			setTimeout(() => setButtonText('Copy'), 6000);
+		}
+
+	}, [setButtonText]);
+
+	return (
+		<PlaceholderIpsum
+			handleSentences={ handleSentences }
+			handleParagraphs={ handleParagraphs }
+			numberOfSentences={ numberOfSentences }
+			numberOfParagraphs={ numberOfParagraphs }
+			handleCopy={ handleCopy }
+			output={ output }
+			buttonText={ buttonText }
+		/>
+	);
+};
+
+export default ConnectedPlaceholderIpsum;
